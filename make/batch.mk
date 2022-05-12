4329537ee601509a3c85f31d8965bb5765930c88
Sungho Park <siminsungho@gmail.com>
2022-05-12 21:28:59 +0900

    remove ubinos config, lsm9ds1_adafruitnrf52840e.

--- make/batch.mk
+++ make/batch.mk
@@ -23,13 +23,6 @@
 	make -f makefile.mk $@ CONFIG_DIR=../config CONFIG_NAME=lsm9ds1_nrf52840dk
 	@echo ""
 	@echo ""
-	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-	@echo ""
-	@echo ""
-	make -f makefile.mk $@ CONFIG_DIR=../config CONFIG_NAME=lsm9ds1_adafruitnrf52840e
-	@echo ""
-	@echo ""
-
 
 
 	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
